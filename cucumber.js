6a8bd4e7c89f9e6d9d3667b4c771060437a0f400
kalai_625 <kalaiyarasan61097@gmail.com>
2022-09-17 10:53:05 +0530

    cucumber report

--- cucumber.js
+++ cucumber.js
@@ -2,7 +2,15 @@ const common = `
     --require setup/assertions.js
     --require setup/hooks.js
     --require step-definitions/**/*.step.js
-`
+
+    --require json:cucumber_report.json
+    --require html:reports/**/*.html
+    --publish-quiet   
+    --format summary
+    --require reporter.js
+    --require progress-bar
+    
+    `
 module.exports = {
   default: `${common} features/**/*.feature`,
 }
--- page-objects/login_page.js
+++ page-objects/login_page.js
@@ -1,14 +1,6 @@
 class LoginPage {
   async navigateToLoginScreen() {
-    //linkinedIN
-    await page.goto('https://www.google.com/?gws_rd=ssl');
-    
-  // await page.waitForURL('https://www.google.com/?gws_rd=ssl');
-  // await page.locator('[aria-label="Search"]').click();
-  // await page.locator('[aria-label="Search"]').fill('linkedin');
-  // await page.locator('[aria-label="Search"]').press('Enter');
-  // await page.waitForURL('https://www.google.com/search?q=linkedin&source=hp&ei=F3QkY4G2D8iOhwPzvam4Cg&iflsig=AJiK0e8AAAAAYySCJyD1wrRHqTm3ghrOJfxacei_YokQ&ved=0ahUKEwjBnZ_jr5n6AhVIx2EKHfNeCqcQ4dUDCAc&uact=5&oq=linkedin&gs_lcp=Cgdnd3Mtd2l6EAMyEQguEIAEELEDEIMBEMcBENEDMgsIABCABBCxAxCDATIOCAAQgAQQsQMQgwEQyQMyBQgAEJIDMhEILhCABBCxAxCDARDHARDRAzIICAAQgAQQsQMyBQgAEIAEMgsIABCABBCxAxCDATIFCAAQgAQyCwgAEIAEELEDEIMBOg4IABCPARDqAhCMAxDlAjoOCC4QjwEQ6gIQjAMQ5QI6BQgAELEDOgUILhCABDoRCC4QgAQQ1AIQiwMQpAMQqAM6CwguEIAEELEDENQCOggILhCABBDUAjoQCC4QsQMQgwEQxwEQ0QMQCjoKCAAQsQMQgwEQClCsD1jxOWC-P2gCcAB4AIABc4gBngaSAQM3LjKYAQCgAQGwAQq4AQI&sclient=gws-wiz');
-
+     await page.goto('https://www.google.com/');
   }
 
   async enterTextSearchBox(){
